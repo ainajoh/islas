f0bcab755c4a8885a9034bc2b0339cbb315949fc
ainajoh <aina.johannessen@gmail.com>
2021-01-11 12:03:40 +0100

    Made a interface for meteogram that handles all the plots

--- weathervis/weathervis/config.py
+++ weathervis/weathervis/config.py
@@ -8,7 +8,7 @@ print(abspath)
 #package_path = os.path.dirname(__file__)
 #os.chdir(dname)
 def setup_directory():
-    projectpath = "../../../../output/"
+    projectpath = dname+"/../../../../output/"
 
     if not os.path.exists(projectpath):
         os.makedirs(projectpath)
