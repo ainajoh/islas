fd0c5bc2b72380e8762e6a8c3cddb4a540676afc
ainajoh <aina.johannessen@gmail.com>
2021-01-13 11:17:00 +0100

    cyclone conf

--- weathervis/weathervis/data/config/config_islas_server.sh
+++ weathervis/weathervis/data/config/config_islas_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 conda_env="conda activate weathervis"
-wpath="/Data/gfi/isomet/projects/ISLAS_aina/tools/githubclones/islas/weathervis/"
+wpath="/home/centos/progs/islas/weathervis/"
 weathervispath="export PYTHONPATH=$PYTHONPATH:$wpath"
 echo "$conda_env"
 $conda_env
