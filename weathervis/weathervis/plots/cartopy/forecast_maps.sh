e8a54bfd59f411e92db47ab974acaafa70d72ef4
Cloud User <centos@islas-operational.novalocal>
2021-03-21 16:41:32 +0000

    try to repair vert met

--- weathervis/weathervis/plots/cartopy/forecast_maps.sh
+++ weathervis/weathervis/plots/cartopy/forecast_maps.sh
@@ -5,8 +5,39 @@ model=$1 # one of AA, MEPS, FP
 runhour=$2
 steps=$3
 
+fclag=100 #100 = 1.00 hour before forecsast is issue
+
+echo "${BASH_VERSINFO:-0}"
+if [ "${BASH_VERSINFO:-0}" -ge 4 ];then
+   modeldatehour=$(date -u --date "today - $(($fclag*60/100)) minutes" +'%Y%m%d%H%M')
+else
+  modeldatehour=$(date -v-$(($fclag*60/100))M -u +%Y%m%d%H%M)
+  #date -v-60M -u +%Y%m%d%H%M
+fi
+echo $modeldatehour
+#modeldatehour="2020030900"
+yy=${modeldatehour:0:4}
+mm=${modeldatehour:4:2}
+dd=${modeldatehour:6:2}
+hh=${modeldatehour:8:2}
+yymmdd="${yy}${mm}${dd}"
+modelrun_hour=$runhour
+modelrun_date=$yymmdd
+
 case "$model" in
     AA)
+	url="https://thredds.met.no/thredds/dodsC/aromearcticarchive/${yy}/${mm}/${dd}/arome_arctic_full_2_5km_${modelrun_date}T${modelrun_hour}Z.nc.html"
+        echo $url
+        web_code=$(curl -sL -w "%{http_code}\n" "$url" -o /dev/null)
+        echo $web_code
+        echo $(($web_code == 200))
+        echo "exit 0" > timeoutwrapper.sh
+        if [ $web_code != 200 ]; then
+           echo "Data not available on web page yet"
+           exit 0
+         fi
+        echo "exit 64" > timeoutwrapper.sh
+	
 	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name Andenes_area
 	automate_all_meteogram_centos.sh --steps_max $steps --modelrun_hour $runhour &
 	automate_all_verticalmeteogram_centos.sh --steps_max $steps --modelrun_hour $runhour &
