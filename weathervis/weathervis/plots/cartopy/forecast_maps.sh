a781161d8e193f7bb332cdf590172d4bd8b0baf6
Cloud User <centos@islas-operational.novalocal>
2021-03-26 22:43:38 +0000

    utils scrapeweb

--- weathervis/weathervis/plots/cartopy/forecast_maps.sh
+++ weathervis/weathervis/plots/cartopy/forecast_maps.sh
@@ -39,11 +39,11 @@ case "$model" in
          fi
         echo "exit 64" > /home/centos/batch/timeoutwrapper_AA_${runhour}.sh
 	
-	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name North_Norway
-	automate_all_meteogram_centos.sh --steps_max $steps --modelrun_hour $runhour &
-	automate_all_verticalmeteogram_centos.sh --steps_max $steps --modelrun_hour $runhour &
-	wait
+	automate_all_meteogram_centos.sh --steps_max $steps --modelrun_hour $runhour 
 	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name Svalbard
+	automate_all_verticalmeteogram_centos.sh --steps_max $steps --modelrun_hour $runhour 
+	#wait
+	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name North_Norway
 	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name Andenes_area
 	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name AromeArctic
 	automate_all_maps.sh --model AromeArctic --steps_max $steps --modelrun_hour $runhour --domain_name NorwegianSea_area
