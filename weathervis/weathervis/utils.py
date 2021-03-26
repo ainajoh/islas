28fe17356b8916f74f71eb10cb050054ecf35d95
Marvin Kahnert <buc010@cyclone.hpc.uib.no>
2021-03-26 23:27:59 +0100

    Added the Extract_from_webpage function to utils. Updates the pcmet2 location with the latest coordinates.

--- weathervis/weathervis/utils.py
+++ weathervis/weathervis/utils.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 import matplotlib.colors as mcolors
+import pandas as pd
+import fileinput
+import sys
 
 def domain_input_handler(dt, model, domain_name, domain_lonlat, file):
 
@@ -166,3 +169,39 @@ def rgb_to_dec(value):
     value: list (length 3) of RGB values
     Returns: list (length 3) of decimal values'''
     return [v/256 for v in value]
+
+
+def Extract_from_webpage():
+    '''
+    Get the latest position of the CMET and update the sites.csv file '''
+
+    url = 'http://www.science.smith.edu/cmet/20180921_ARC_02/flight_data.txt'
+    cr = pd.read_csv(url,sep='\s+',names=['T1','lat','lon','Unknown','T2','T3','RH'])
+    
+    # now get latest pair of lon/lat that is not NaN
+    cr2 = cr.copy()
+    cr2 = cr2[(cr2['lon'].notnull()) & (cr2['lat'].notnull())]
+    
+    lastlon = np.array(cr2['lon'].tail(1))[0]
+    lastlat = np.array(cr2['lat'].tail(1))[0]
+    
+    # now go to sites.csv file and modify the pcmet2 point
+    file = '/Data/gfi/isomet/projects/ISLAS_marvin/islas/weathervis/weathervis/data/sites.csv'
+    read_file_name = file + '.old'
+    searchExp = 'pcmet2'
+    replaceExp = 'pcmet2;'+str(lastlat)+';'+str(lastlon)+';None;None;None;None'
+        
+    # find line number, delete it, append new location
+    
+    os.rename(file, read_file_name)
+    
+    with open(read_file_name, 'r') as read_file:
+        with open(file, 'a') as write_file:
+    
+            for n, line in enumerate(read_file, 1):
+                if searchExp in line:
+                    write_file.write(replaceExp)
+                    print('Repleaced line no', n)
+                else:
+                    write_file.write(line)
+    os.remove(read_file_name)
