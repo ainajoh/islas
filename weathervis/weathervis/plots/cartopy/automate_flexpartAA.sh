82079ea6f168765b06e635e7f01134e3dd33fde4
Aina Johannessen <cat010@cyclone.hpc.uib.no>
2021-03-26 02:38:28 +0100

    comitt flex

--- weathervis/weathervis/plots/cartopy/automate_flexpartAA.sh
+++ weathervis/weathervis/plots/cartopy/automate_flexpartAA.sh
@@ -4,32 +4,6 @@ source ~/.bashrc
 function converting {
   here=$( pwd )
   # convert to smaller image size and transfer to web disk
-<<<<<<< HEAD
-  cd /Data/gfi/isomet/projects/ISLAS_aina/output/weathervis/
-  if ! [ -d $1 ]; then
-     mkdir $1
-  fi
-  cd /Data/gfi/isomet/projects/ISLAS_aina/output/weathervis/$1
-  mkdir small
-  for f in *.png; do 
-    echo $f
-    convert -scale 40% $f small/$f
-  done
-  wait
-  #if ! [ -d /home/centos/www/gfx/$1 ]; then
-  #  mkdir /home/centos/www/gfx/$1
-  #fi
-  #cp small/* /home/centos/www/gfx/$1
-  #rm -rf ~/output/weathervis/$1
-  #sudo chown -R centos:apache /home/centos/www/gfx/$1  
-  ## transfer to webserver
-  #if [[ "$HOSTNAME" == *"islas-operational.novalocal"* ]]; then
-  #  copy="scp -i /home/centos/.ssh/islas-key.pem /home/centos/www/gfx/$1/FLEXPART_AA* 158.39.201.233:/home/centos/www/gfx/$1/"
-  #  echo $copy
-  #  $copy
-  #fi
-  #cd $here
-=======
   cd /home/centos/output/weathervis/$1
   mkdir small
   for f in *.png; do 
@@ -48,7 +22,6 @@ function converting {
     $copy
   fi
   cd $here
->>>>>>> c091606fd6ab32c1c3a0be9d42ec4a6d840789da
 }
 
 #dirname=$( pwd )
@@ -92,17 +65,10 @@ yymmdd="${yy}${mm}${dd}"
 modelrun_date=$yymmdd
 modelrun_hour="00"
 model=("AromeArctic")
-<<<<<<< HEAD
-steps_max=(66)
-domain_name="None"
-release_name="NYA"
-domain_name=("AromeArctic" "North_Norway" "Svalbard" "Andenes_area" NorwegianSea_area)
-=======
 steps_max=(1)
 domain_name="None"
 release_name="NYA"
 
->>>>>>> c091606fd6ab32c1c3a0be9d42ec4a6d840789da
 while [ $# -gt 0 ]; do
   case "$1" in
     --model)
@@ -129,11 +95,7 @@ while [ $# -gt 0 ]; do
     ;;
     --domain_name)
     if [[ "$1" != *=* ]]; then shift;# Value is next arg if no `=`
-<<<<<<< HEAD
-    domain_name=("${1#*=}")
-=======
     domain_name="${1#*=}"
->>>>>>> c091606fd6ab32c1c3a0be9d42ec4a6d840789da
     fi
     ;;
     --release_name)
@@ -149,12 +111,7 @@ while [ $# -gt 0 ]; do
   esac
   shift
 done
-<<<<<<< HEAD
-#domain_name=($domain_name)
-echo domain_name
-=======
 
->>>>>>> c091606fd6ab32c1c3a0be9d42ec4a6d840789da
 echo $model
 echo $modelrun_date
 echo $modelrun_hour
@@ -177,10 +134,6 @@ echo $modelrun
 #fi
 
 
-<<<<<<< HEAD
-#modelrun=("2020022712" "2020022812" "2020022912" "2020030112" "2020030212" "2020030312" "2020030412" "2020030512" "2020030612" "2020030712" "2020030812" "2020030912" "2020031012" "2020031112" "2020031212" "2020031312" "2020031412" "2020031516" "2020031612")
-#modelrun=("2021030400")
-=======
 #modelrun=("2020100300") "2020021712" "2020021812" "2020021912"
 #modelrun=( "2020022712" "2020022812" "2020022912")
 #modelrun=("2020022012" "2020022112" )
@@ -203,14 +156,13 @@ modelrun=("2021030400")
 for md in ${model[@]}; do
   echo $md
   for ((i = 0; i < ${#modelrun[@]}; ++i)); do
-	  for dom in ${domain_name[@]}; do
-    		cd /Data/gfi/isomet/projects/ISLAS_aina/tools/githubclones/islas/weathervis/weathervis/plots/cartopy
-                runstring_FP="python flexpart_AA.py --datetime ${modelrun[i]} --steps 0 $steps_max --model $md --domain_name $dom"
-		
-    		echo $runstring_FP
-    		$runstring_FP
-    		converting $modelrun
-   done
+    runstring_FP="python flexpart_AA.py --datetime ${modelrun[i]} --steps 0 ${steps_max[i]} --model $md --domain_name $domain_name"
+
+    echo $runstring_FP
+    $runstring_FP
+    converting $modelrun
+
+  done
 done
 
 # fin
